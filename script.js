909004be7faaa09b69d54fcac6ff4cfc1bd83f3a
Maggie <muriithinyawira13@gmail.com>
2022-11-18 13:42:56 +0300

    Adding images and navbar

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Welcome to my portfolio";
-
-document.querySelector("body").appendChild(h2);
+const flow = document.getElementById("container");
+flow.insertBefore(h2, flow.children[0]);
