7406e7ee4dc3f526b197a2dbec1b821eeb414ed2
Maggie <muriithinyawira13@gmail.com>
2022-11-19 13:10:07 +0300

    Adding links and editing layout

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Welcome to my portfolio";
+
 const flow = document.getElementById("container");
 flow.insertBefore(h2, flow.children[0]);
+
